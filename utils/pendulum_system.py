aa3e182c58c636243fd974678b10c3b822b46a23
Gregory Barber <42629337+gbarber94@users.noreply.github.com>
2021-03-19 14:38:35 -0400

    Update pendulum_system.py

--- utils/pendulum_system.py
+++ utils/pendulum_system.py
@@ -37,6 +37,77 @@ def integrate_model(model, t_span, y0, lv,**kwargs):
         return dx
 
     return solve_ivp(fun=fun, t_span=t_span, y0=y0, **kwargs)
+  
+def get_one_trajectory(t_span=[0,5], timescale=15, radius=None, y0= np.array([2,0]), noise_std=0.1, n_points = 50, **kwargs):
+    
+    t_eval = np.linspace(t_span[0], t_span[1], n_points)
+    
+    # ODE solver
+    pen_sol = solve_ivp(fun=dynamics_fn, 
+                           t_span=t_span, 
+                           y0=y0, 
+                           t_eval=t_eval, 
+                           rtol=1e-10, 
+                           **kwargs)
+
+    q, p = pen_sol['y'][0], pen_sol['y'][1]
+
+    dydt = [dynamics_fn(None, y) for y in pen_sol['y'].T]
+    dydt = np.stack(dydt).T
+    dqdt, dpdt = np.split(dydt,2)
+
+    return q, p, dqdt,dpdt,t_eval
+
+
+def gen_data(n_to_gen = 100, len_interval = [0.1, 0.5],y0= np.array([1,0])):
+
+  x = np.zeros([n_to_gen,50])
+  aux_vars = np.zeros([n_to_gen,2])
+  qp_dot = np.zeros([n_to_gen,2])
+  q_l = []
+  p_l = []
+  qdot_l = []
+  pdot_l = []
+  length = []
+  t_eval_l = []
+
+  for i in range(n_to_gen):
+    #print(i)
+
+    pen_params['l'] = ((len_interval[1] - len_interval[0]) * np.random.rand(1) + len_interval[0])[0]
+    length.append(pen_params['l'])
+    q, p, qdot,pdot, t_e = get_one_trajectory(t_span=[0,10],n_points=100, y0 = y0)
+    t_to_eval = np.random.randint(0,49)
+
+
+    # Encoder input
+    x[i] = q[:50] # trj length 50
+
+    # Aux latent vars
+    #aux_vars[i][:,0]
+    aux_vars[i] = q[50:][t_to_eval], p[50:][t_to_eval]  # q,p length 2
+
+    # GT output
+    qp_dot[i]   = qdot[0][50:][t_to_eval], pdot[0][50:][t_to_eval] # q_dot, p_dot length 2
+
+    # GT
+    q_l.append(q)
+    p_l.append(p)
+    qdot_l.append(qdot[0])
+    pdot_l.append(pdot[0])
+    t_eval_l.append(t_to_eval)
+
+  data = {'x': x,
+        'aux_vars': aux_vars,
+        'qp_dot': qp_dot,
+        'q': q_l,
+        'p': p_l,
+        'qdot': qdot_l,
+        'pdot': pdot_l,
+        't_eval': t_eval_l,
+        'length': length}
+
+  return data
 
 
 def add_noies(trj,sigma = .03):
